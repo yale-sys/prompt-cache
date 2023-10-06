8350d60eda47c42bb07f011436f08e212d22d212
Seung-seob Lee <shsym1989@gmail.com>
2023-10-06 01:04:55 +0000

    Quick fix for dataset download script

--- dataset_download.py
+++ dataset_download.py
@@ -2,6 +2,6 @@ from datasets import load_dataset
 
 ## I will simply list up datasets below
 
-print("Multi News\n", load_dataset('multi_news')[0])
+print("Multi News\n", load_dataset('multi_news')['train'][0])
 
-print("MS_Marco", load_dataset('ms_marco', 'v2.1')[0])
+print("MS_Marco", load_dataset('ms_marco', 'v2.1')['train'][0])
