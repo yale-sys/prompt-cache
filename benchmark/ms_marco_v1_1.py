22eb27c86c3d05300f6f03644e10bd63ec909605
Seung-seob Lee <shsym1989@gmail.com>
2023-10-12 14:23:52 +0000

    prompt update for ms marco

--- benchmark/ms_marco_v1_1.py
+++ benchmark/ms_marco_v1_1.py
@@ -13,8 +13,8 @@ _document_schema_name = "multi_document_qna"
 _document_header = "Document"
 _document_dataset = "train"
 _document_system_description = "Dialogues between a user and an AI about the document provided by the user with the aim of being helpful, aware, and accurate."
-_document_assistant_description = "Sure. I have read the documents separated by \\n. Give me any instructions regarding query information based on the documents, and I will try to follow them."
-_document_user_summary = "Among the list of given documents separated by \\n, find the document that is most useful to answer the query and return its index starting at 0. All documents are unrelated, return -1."  # Take a deep breath and think step-by-step.
+_document_assistant_description = "Sure. I have read the documents separated by comma. Give me any instructions regarding query information based on the documents, and I will try to follow them."
+_document_user_summary = "Among the list of given documents separated by a comma, find the document that is most useful to answer the query and return its index starting at [0]. All documents are unrelated, return [-1]. Do not reply using a complete sentence, and only give the answer in the following format: [1]"  # Take a deep breath and think step-by-step.
 
 MAX_DOCUMENT_LENGTH = 2560
 
@@ -58,12 +58,13 @@ class MSMarcoV1(Benchmark):
 
             # Set the user description
             builder.set_user_description("")    # _document_user_description
+            # builder.add_document_module("DOC", "The given documents are the target for searching to answer the query. They can contain multiple sentences.")
 
             # Add document modules
             # - we need to collect passages into document str, separated by newline or \n
             document_str = ""
-            for passage in self.dataset[_document_dataset][document_idx]["passages"]["passage_text"]:
-                document_str += f"{passage}\\n"
+            for p_idx, passage in enumerate(self.dataset[_document_dataset][document_idx]["passages"]["passage_text"]):
+                document_str += f"\"[{p_idx}] {passage},\n"
             document_str = document_str.replace("’", "'").replace("”", '"').replace("“", '"').replace("‘", "'").replace("…", "...").replace("–", "-")
             module_str = f"{_document_header}{document_idx}"
             builder.add_document_module(f"{module_str}", document_str)
