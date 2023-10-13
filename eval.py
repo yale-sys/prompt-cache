2c6635cee2eb79abfd42e74c3e297eaa2a67a79f
In Gim <in.gim@yale.edu>
2023-10-13 14:22:46 -0700

    Add latency measure code

--- eval.py
+++ eval.py
@@ -16,6 +16,7 @@ from benchmark.ms_marco_v1_1 import MSMarcoV1
 
 BENCHMARK_PATH = "./benchmark"
 
+
 class Eval:
     def __init__(self, llm_config_path, dataset, enable_cache):
         with open("./config/dataset_maxlen.json", 'r') as f:
@@ -154,6 +155,46 @@ class Eval:
             json.dump(results, f)
             f.write("\n")
 
+    def run_latency_eval(self):
+
+        for entry in self.dataset.entries:
+            schema_file_path = os.path.join(SCHEMA_FILE_DIRECTORY, self.dataset.dataset_name, entry.schema)
+
+            self.cache_engine.add_schema(read_file(schema_file_path, self.preproc))
+
+            prompt = Prompt(entry.prompt, self.preproc)
+
+            no_cache = not self.enable_cache
+
+            token_ids, position_ids, cache_time, cache = self.cache_engine.process(prompt, no_cache=no_cache,
+                                                                                   return_full_position_ids=self.lm.use_full_position_ids)
+
+            input_ids = torch.tensor([token_ids], device=self.lm.device, dtype=torch.long)
+            position_ids = torch.tensor([position_ids], device=self.lm.device, dtype=torch.long)
+            # print(len(position_ids[0]))
+
+            # add redundant batch dim
+            if cache is not None:
+                cache = [(k[0].unsqueeze(0), k[1].unsqueeze(0)) for k in cache]
+
+            start = torch.cuda.Event(enable_timing=True)
+            end = torch.cuda.Event(enable_timing=True)
+
+            start.record()
+            out = self.lm(input_ids=input_ids,
+                          position_ids=position_ids,
+                          past_key_values=cache,
+                          use_cache=True)
+            end.record()
+            torch.cuda.synchronize()
+            response_time = start.elapsed_time(end)
+
+            result = {
+                "cache_time": cache_time,
+                "response_time": response_time,
+            }
+            self.store_results(result)
+
     def run(self, cache_batch_size, split):
         entry_count = self.dataset.get_entry_count()
         split_count = entry_count // split[1]
@@ -210,9 +251,13 @@ class Eval:
 
 
 def main(llm_config_path: str = os.path.join('./', "config/llm_config_llama2.json"),
-         dataset: str = "2wikimqa", enable_cache=False, cache_batch_size=1, split=(0, 1)):
+         dataset: str = "repobench-p", enable_cache=False, cache_batch_size=1, split=(0, 1), test_latency=False):
     eval = Eval(llm_config_path, dataset, enable_cache)
-    eval.run(cache_batch_size, split)
+
+    if test_latency:
+        eval.run_latency_eval()
+    else:
+        eval.run(cache_batch_size, split)
 
 
 if __name__ == "__main__":
